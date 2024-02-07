96d057cbce69c45ab80b9c799949ce01d6fceb6c
thalesreis7 <thales.reisfurtado@gmail.com>
2024-02-07 14:33:52 -0300

    Update Project

---
+++ src/Layouts/App.jsx
@@ -0,0 +1,18 @@
+import Header from '../Components/Pages/Header'
+import Main from '../Components/Pages/Main'
+import Footer from '../Components/Pages/Footer'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+     <Header/>
+     <Main/>
+     <Footer/>
+    </>
+  )
+
+  
+}
+
+
